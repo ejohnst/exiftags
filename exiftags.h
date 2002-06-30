cc41deea5a4ab2156806028b410dd8995a064f0b
Eric M. Johnston <ejohnst@postal.net>
2002-06-30 03:59:44 -0500

    Updated copyright.

--- exiftags.h
+++ exiftags.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, Eric M. Johnston <emj@postal.net>
+ * Copyright (c) 2001, 2002, Eric M. Johnston <emj@postal.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -7,8 +7,15 @@
  * are met:
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 2. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Eric M. Johnston.
+ * 4. Neither the name of the author nor the names of any co-contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -22,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: exiftags.h,v 1.1 2002/01/20 23:39:32 ejohnst Exp $
+ * $Id: exiftags.h,v 1.2 2002/06/30 08:59:44 ejohnst Exp $
  */
 
 
