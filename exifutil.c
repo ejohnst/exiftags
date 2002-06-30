92d3a0e86fdb95cb3e2862a3da97d60c73473471
Eric M. Johnston <ejohnst@postal.net>
2002-06-30 04:00:54 -0500

    Updated copyright. Updated header. Added readifd() and readifds().

--- exifutil.c
+++ exifutil.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, Eric M. Johnston <emj@postal.net>
+ * Copyright (c) 2001, 2002, Eric M. Johnston <emj@postal.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -7,8 +7,15 @@
  * are met:
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 2. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Eric M. Johnston.
+ * 4. Neither the name of the author nor the names of any co-contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -22,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: exifutil.c,v 1.2 2002/01/20 23:59:09 ejohnst Exp $
+ * $Id: exifutil.c,v 1.3 2002/06/30 09:00:54 ejohnst Exp $
  */
 
 /*
@@ -34,7 +41,7 @@
 #include <string.h>
 #include <errno.h>
 
-#include "exifdump.h"
+#include "exiftags.h"
 #include "exif.h"
 
 
@@ -143,4 +150,85 @@ childprop(struct exifprop *parent)
 	parent->next = prop;
 
 	return (prop);
+}
+
+
+/*
+ * Allocate and read an individual IFD.  Takes the beginning and end of the
+ * Exif buffer, returns the IFD and an offset to the next IFD.
+ */
+u_int32_t
+readifd(unsigned char *b, struct ifd **dir)
+{
+	u_int32_t ifdsize;
+
+	/*
+	 * Verify that we have a valid offset.  Some maker note IFDs prepend
+	 * a string and will screw us up otherwise (e.g., Olympus).
+	 * (Number of directory entries is in the first 2 bytes.)
+	 */
+
+	if (b + 2 > etiff) {
+		*dir = NULL;
+		return (0);
+	}
+
+	*dir = (struct ifd *)malloc(sizeof(struct ifd));
+	if (!*dir)
+		exifdie((const char *)strerror(errno));
+
+	(*dir)->next = NULL;
+	(*dir)->num = exif2byte(b);
+	(*dir)->tag = EXIF_T_UNKNOWN;
+	ifdsize = (*dir)->num * sizeof(struct field);
+	b += 2;
+
+	/* Sanity check our sizes. */
+
+	if (b + ifdsize > etiff) {
+		free(*dir);
+		*dir = NULL;
+		return (0);
+	}
+
+	/* Point to our array of fields. */
+
+	(*dir)->fields = (struct field *)b;
+
+	/*
+	 * While we're here, find the offset to the next IFD.
+	 *
+	 * XXX Note that this offset isn't always going to be valid.  It
+	 * seems that some camera implementations of Exif ignore the spec
+	 * and do not include the offset for all IFDs (e.g., maker note).
+	 * Therefore, it may be necessary to call readifd() directly (in
+	 * leiu of readifds()) to avoid problems when reading these non-
+	 * standard IFDs.
+	 */
+
+	return ((b + ifdsize + 4 > etiff) ? 0 : exif4byte(b + ifdsize));
+}
+
+
+/*
+ * Read a chain of IFDs.  Takes the IFD offset and returns the first
+ * node in a chain of IFDs.  Note that it can return NULL.
+ */
+struct ifd *
+readifds(u_int32_t offset)
+{
+	struct ifd *firstifd, *curifd;
+
+	/* Fetch our first one. */
+
+	offset = readifd(btiff + offset, &firstifd);
+	curifd = firstifd;
+
+	/* Fetch any remaining ones. */
+
+	while (offset) {
+		offset = readifd(btiff + offset, &(curifd->next));
+		curifd = curifd->next;
+	}
+	return (firstifd);
 }
