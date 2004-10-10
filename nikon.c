12629de14d0590da8b54c12cd81a6d8f61952c9e
Eric M. Johnston <ejohnst@postal.net>
2004-10-09 22:48:38 -0500

    Minor overhaul: consolidated two different tagsets (not sure why I didn't before), and updated/corrected a few things using info from http://www.ozhiker.com/electronics/pjmt/jpeg_info/nikon_mn.html.
    Still needs a little work.

--- nikon.c
+++ nikon.c
@@ -29,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: nikon.c,v 1.17 2003/08/16 03:02:16 ejohnst Exp $
+ * $Id: nikon.c,v 1.18 2004/10/10 03:48:38 ejohnst Exp $
  */
 
 /*
@@ -37,6 +37,10 @@
  *
  * Some information for Nikon D1X support obtained from JoJoThumb, version
  * 2.7.2 (http://www.jojosoftware.de/jojothumb/).
+ *
+ * Updated with data from
+ * http://www.ozhiker.com/electronics/pjmt/jpeg_info/nikon_mn.html.
+ *
  */
 
 #include <stdio.h>
@@ -55,6 +59,9 @@ static struct descrip nikon_quality[] = {
 	{ 4,	"SXGA Basic" },
 	{ 5,	"SXGA Normal" },
 	{ 6,	"SXGA Fine" },
+	{ 10,	"UXGA Basic" },
+	{ 11,	"UXGA Normal" },
+	{ 12,	"UXGA Fine" },
 	{ -1,	"Unknown" },
 };
 
@@ -95,12 +102,12 @@ static struct descrip nikon_ccd[] = {
 
 static struct descrip nikon_white[] = {
 	{ 0,	"Auto" },
-	{ 2,	"Preset" },
-	{ 4,	"Daylight" },
-	{ 5,	"Incandescent" },
-	{ 5,	"Fluorescent" },
+	{ 1,	"Preset" },
+	{ 2,	"Daylight" },
+	{ 3,	"Incandescent" },
+	{ 4,	"Fluorescent" },
 	{ 5,	"Cloudy" },
-	{ 5,	"Speedlight" },
+	{ 6,	"Speedlight" },
 	{ -1,	"Unknown" },
 };
 
@@ -114,40 +121,10 @@ static struct descrip nikon_convert[] = {
 };
 
 
-/* Maker note IFD tags. */
+/* Old school Nikon "lookup" maker note IFD tags. */
 
 static struct exiftag nikon_tags0[] = {
-	{ 0x0002, TIFF_SHORT, 2, ED_UNK, "NikonISOSet",
-	  "ISO Setting", NULL },
-	{ 0x0003, TIFF_ASCII, 0, ED_IMG, "NikonColor",
-	  "Color Mode", NULL },
-	{ 0x0004, TIFF_ASCII, 0, ED_IMG, "NikonQuality",
-	  "Image Quality", NULL },
-	{ 0x0005, TIFF_ASCII, 0, ED_IMG, "NikonWhiteBal",
-	  "White Balance", NULL },
-	{ 0x0006, TIFF_ASCII, 0, ED_IMG, "NikonSharp",
-	  "Image Sharpening", NULL },
-	{ 0x0007, TIFF_ASCII, 0, ED_IMG, "NikonFocusMode",
-	  "Focus Mode", NULL },
-	{ 0x0008, TIFF_ASCII, 0, ED_IMG, "NikonFlashSet",
-	  "Flash Setting", NULL },
-	{ 0x000f, TIFF_ASCII, 0, ED_IMG, "NikonISOSelect",
-	  "ISO Selection", NULL },
-	{ 0x0080, TIFF_ASCII, 0, ED_IMG, "NikonImgAdjust",
-	  "Image Adjustment", NULL },
-	{ 0x0082, TIFF_ASCII, 0, ED_IMG, "NikonAdapter",
-	  "Lens Adapter", NULL },
-	{ 0x0085, TIFF_ASCII, 0, ED_IMG, "NikonFocusDist",
-	  "Focus Distance", NULL },
-	{ 0x0086, TIFF_ASCII, 0, ED_IMG, "NikonDigiZoom",
-	  "Digital Zoom", NULL },
-	{ 0xffff, TIFF_UNKN, 0, ED_UNK, "Unknown",
-	  "Nikon Unknown", NULL },
-};
-
-
-static struct exiftag nikon_tags1[] = {
-	{ 0x0003, TIFF_SHORT, 1, ED_UNK, "NikonQuality",
+	{ 0x0003, TIFF_SHORT, 1, ED_IMG, "NikonQuality",
 	  "Image Quality", nikon_quality },
 	{ 0x0004, TIFF_SHORT, 1, ED_IMG, "NikonColor",
 	  "Color Mode", nikon_color },
@@ -168,11 +145,13 @@ static struct exiftag nikon_tags1[] = {
 };
 
 
-static struct exiftag nikon_tags2[] = {
+/* Newer Nikon ASCII maker note IFD tags. */
+
+static struct exiftag nikon_tags1[] = {
 	{ 0x0001, TIFF_UNDEF, 4, ED_VRB, "NikonVersion",
 	  "Nikon Note Version", NULL },
-	{ 0x0002, TIFF_SHORT, 2, ED_UNK, "NikonISOSetting",
-	  "ISO Setting", NULL },
+	{ 0x0002, TIFF_SHORT, 2, ED_UNK, "NikonISOUsed",
+	  "ISO Speed Used", NULL },
 	{ 0x0003, TIFF_ASCII, 0, ED_IMG, "NikonColor",
 	  "Color Mode", NULL },
 	{ 0x0004, TIFF_ASCII, 0, ED_IMG, "NikonQuality",
@@ -181,28 +160,98 @@ static struct exiftag nikon_tags2[] = {
 	  "White Balance", NULL },
 	{ 0x0006, TIFF_ASCII, 0, ED_IMG, "NikonImgSharp",
 	  "Image Sharpening", NULL },
-	{ 0x0007, TIFF_ASCII, 0, ED_IMG, "NikonFocus",
+	{ 0x0007, TIFF_ASCII, 0, ED_IMG, "NikonFocusMode",
 	  "Focus Mode", NULL },
-	{ 0x0008, TIFF_ASCII, 0, ED_IMG, "NikonFlash",
+	{ 0x0008, TIFF_ASCII, 0, ED_IMG, "NikonFlashSet",
 	  "Flash Setting", NULL },
+	{ 0x0009, TIFF_ASCII, 0, ED_IMG, "NikonAutoFlash",
+	  "Auto Flash Mode", NULL },
 	{ 0x000b, TIFF_UNKN, 0, ED_UNK, "NikonWhiteBalBias",
 	  "White Balance Bias", NULL },
-	{ 0x0081, TIFF_ASCII, 0, ED_IMG, "NikonImgAdjust",
+	{ 0x000f, TIFF_ASCII, 0, ED_IMG, "NikonISOSelect",
+	  "ISO Selection", NULL },
+	{ 0x0012, TIFF_SHORT, 1, ED_IMG, "NikonFlashComp",
+	  "Flash Compensation", NULL },
+	{ 0x0013, TIFF_SHORT, 2, ED_IMG, "NikonISOReq",
+	  "ISO Speed Requested", NULL },
+	{ 0x0018, TIFF_SHORT, 1, ED_IMG, "NikonFlashBrackComp",
+	  "Flash Bracket Compensation", NULL },
+	{ 0x0019, TIFF_SHORT, 1, ED_IMG, "NikonAEBrackComp",
+	  "AE Bracket Compensation", NULL },
+	{ 0x0080, TIFF_ASCII, 0, ED_IMG, "NikonImgAdjust",
 	  "Image Adjustment", NULL },
-	{ 0x0088, TIFF_UNDEF, 0, ED_UNK, "NikonAutoFocus",
+	{ 0x0081, TIFF_ASCII, 0, ED_IMG, "NikonToneComp",
+	  "Tone Compensation", NULL },
+	{ 0x0082, TIFF_ASCII, 0, ED_IMG, "NikonLensAdapter",
+	  "Lens Adapter", NULL },
+	{ 0x0083, TIFF_ASCII, 0, ED_IMG, "NikonLensType",
+	  "Lens Type", NULL },
+	{ 0x0084, TIFF_ASCII, 0, ED_IMG, "NikonLensRange",
+	  "Lens Range", NULL },
+	{ 0x0085, TIFF_ASCII, 0, ED_IMG, "NikonFocusDist",
+	  "Focus Distance", NULL },
+	{ 0x0086, TIFF_ASCII, 0, ED_IMG, "NikonDigiZoom",
+	  "Digital Zoom", NULL },
+	{ 0x0087, TIFF_SHORT, 1, ED_IMG, "NikonFlashUsed",
+	  "Flash Used", NULL },
+	{ 0x0088, TIFF_SHORT, 1, ED_IMG, "NikonAutoFocus",
 	  "Auto Focus Position", NULL },
+	{ 0x0089, TIFF_SHORT, 1, ED_IMG, "NikonBrackShoot",
+	  "Bracketing/Shooting Mode", NULL },
+	{ 0x008d, TIFF_ASCII, 0, ED_IMG, "NikonColorMode",
+	  "Color Mode 2", NULL },
+	{ 0x008f, TIFF_ASCII, 0, ED_IMG, "NikonLighting",
+	  "Lighting Type", NULL },
 	{ 0x0092, TIFF_UNDEF, 0, ED_UNK, "NikonHueAdjust",
 	  "Hue Adjustment", NULL },
-	{ 0xffff, TIFF_UNKN, 0, ED_UNK, "NikonUnknown",
+	{ 0x0094, TIFF_SHORT, 1, ED_IMG, "NikonSaturate",
+	  "Saturation", NULL },
+	{ 0x0095, TIFF_ASCII, 0, ED_IMG, "NikonNoiseReduce",
+	  "Noise Reduction", NULL },
+	{ 0x00a7, TIFF_SHORT, 1, ED_IMG, "NikonAcuations",
+	  "Camera Actuations", NULL },
+	{ 0x00a9, TIFF_ASCII, 0, ED_IMG, "NikonImageOpt",
+	  "Image Optimization", NULL },
+	{ 0x00aa, TIFF_ASCII, 0, ED_IMG, "NikonSaturate2",
+	  "Saturation 2", NULL },
+	{ 0x00ab, TIFF_ASCII, 0, ED_IMG, "NikonDigiProg",
+	  "Digital Vari-Program", NULL },
+	{ 0xffff, TIFF_UNKN, 0, ED_UNK, "Unknown",
 	  "Nikon Unknown", NULL },
 };
 
 
 /*
- * Process normal Nikon maker note tags.
+ * Process older Nikon maker note tags.
  */
 static void
 nikon_prop0(struct exifprop *prop, struct exiftags *t)
+{
+	u_int32_t a, b;
+
+	switch (prop->tag) {
+
+	/* Digital zoom. */
+
+	case 0x000a:
+		a = exif4byte(t->md.btiff + prop->value, t->md.order);
+		b = exif4byte(t->md.btiff + prop->value + 4, t->md.order);
+
+		if (!a) {
+			snprintf(prop->str, 31, "None");
+			prop->lvl = ED_VRB;
+		} else
+			snprintf(prop->str, 31, "x%.1f", (float)a / (float)b);
+		break;
+	}
+}
+
+
+/*
+ * Process newer Nikon maker note tags.
+ */
+static void
+nikon_prop1(struct exifprop *prop, struct exiftags *t)
 {
 	u_int32_t a, b;
 
@@ -238,42 +287,21 @@ nikon_prop0(struct exifprop *prop, struct exiftags *t)
 
 
 /*
- * Process Nikon maker note tags that start from an offset.
- */
-static void
-nikon_prop1(struct exifprop *prop, struct exiftags *t)
-{
-	u_int32_t a, b;
-
-	switch (prop->tag) {
-
-	/* Digital zoom. */
-
-	case 0x000a:
-		a = exif4byte(t->md.btiff + prop->value, t->md.order);
-		b = exif4byte(t->md.btiff + prop->value + 4, t->md.order);
-
-		if (!a) {
-			snprintf(prop->str, 31, "None");
-			prop->lvl = ED_VRB;
-		} else
-			snprintf(prop->str, 31, "x%.1f", (float)a / (float)b);
-		break;
-	}
-}
-
-
-/*
  * Process Nikon maker note tags.
  */
 void
 nikon_prop(struct exifprop *prop, struct exiftags *t)
 {
 
-	if (prop->tagset == nikon_tags1)
-		nikon_prop1(prop, t);
-	else
+	if (prop->tagset == nikon_tags0) {
 		nikon_prop0(prop, t);
+		return;
+	}
+
+	if (prop->tagset == nikon_tags1) {
+		nikon_prop1(prop, t);
+		return;
+	}
 }
 
 
@@ -291,7 +319,8 @@ nikon_ifd(u_int32_t offset, struct tiffmeta *md)
 	mkrmd = *md;
 
 	/*
-	 * Seems that some Nikon maker notes start with an ID string.
+	 * Seems that some Nikon maker notes start with an ID string and
+	 * a version of some sort.
 	 */
 
 	if (!strcmp((const char *)b, "Nikon")) {
@@ -299,7 +328,7 @@ nikon_ifd(u_int32_t offset, struct tiffmeta *md)
 
 		switch (exif2byte(b, md->order)) {
 		case 0x0001:
-			readifd(offset + 8, &myifd, nikon_tags1, &mkrmd);
+			readifd(offset + 8, &myifd, nikon_tags0, &mkrmd);
 			return (myifd);
 
 		case 0x0200:
@@ -332,11 +361,19 @@ nikon_ifd(u_int32_t offset, struct tiffmeta *md)
 			b += 2;
 
 			readifd(exif4byte(b, mkrmd.order), &myifd,
-			    nikon_tags2, &mkrmd);
+			    nikon_tags1, &mkrmd);
 			return (myifd);
+
+		default:
+			exifwarn("Nikon maker note version not supported");
+			return (NULL);
 		}
 	}
 
-	readifd(offset, &myifd, nikon_tags0, &mkrmd);
+	/*
+	 * Others are just normal IFDs.
+	 */
+
+	readifd(offset, &myifd, nikon_tags1, &mkrmd);
 	return (myifd);
 }
